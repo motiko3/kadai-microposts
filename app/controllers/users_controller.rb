80124be2e8c7e052af76bdbbec11d3075d8a6962
motiko <motiko@example.com>
2019-12-06 00:50:14 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show,:index]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
