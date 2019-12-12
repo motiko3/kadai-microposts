8cbc83d6c470d8fddea8a88df08c2b8e33e31ceb
motiko <motiko@example.com>
2019-12-12 10:01:15 +0000

    add likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -30,7 +32,18 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
+  def favorite(other_user_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_user_micropost.id)
+  end
   
+  def unfavorite(other_user_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_user_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def likes?(other_user_micropost)
+    self.likes.include?(other_user_micropost)
+  end
   
   
 end
